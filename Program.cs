255ee9ad91b132b342e0dde19efcca4cf6b576c1
Janis Bürkle <bks@teamtechnik.com>
2023-12-14 11:31:33 +0100

    First

--- Program.cs
+++ Program.cs
@@ -1,92 +1,64 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Net;
-using System.Text;
-using System.Threading;
+using System.Runtime.InteropServices;
 
-class Program
+internal class Program
 {
-    static void Main()
-    {
-        // Die URL, auf der der Server lauscht
-        string url = "http://localhost:8080/";
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetForegroundWindow();
 
-        // Erstelle eine Instanz von HttpListener
-        using (HttpListener listener = new HttpListener())
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+    private const uint WM_CHAR = 0x0102;
+    private static void Main()
+    {
+        var url = "http://localhost:8080/";
+
+        using (var listener = new HttpListener())
         {
-            // Füge die URL zur Liste der Prefixe hinzu
             listener.Prefixes.Add(url);
 
-            // Starte den Server
             listener.Start();
             Console.WriteLine($"Server gestartet. Lauscht auf {url}");
 
             try
             {
-                // Warte auf Anfragen
                 while (true)
                 {
-                    // Akzeptiere eine eingehende Anfrage
-                    HttpListenerContext context = listener.GetContext();
-
-                    // Verarbeite die Anfrage in einem separaten Thread, um den Server weiterhin zu akzeptieren
-                    ThreadPool.QueueUserWorkItem((o) =>
+                    var context = listener.GetContext();
+                    ThreadPool.QueueUserWorkItem(o =>
                     {
-                        // Hier füge eine Ausgabe in die Konsole ein
-                        Console.WriteLine("Button wurde gedrückt!");
-
-                        // Pfade zur Batch-Datei und zum Arbeitsverzeichnis
-                        string batchFilePath = @"C:\Users\bks\Downloads\test.bat";
-                        string workingDirectory = @"C:\Users\bks\Downloads\";
-
-                        try
+                        var parameterValue = context.Request.QueryString["parameter"];
+                        Console.WriteLine($"Button wurde gedrückt! Parameter: {parameterValue}");
+                        
+                        if (parameterValue == "startServer")
                         {
-                            // Erstelle einen neuen Prozess
-                            using (Process process = new Process())
+                            try
                             {
-                                // Konfiguriere den Prozess
-                                ProcessStartInfo startInfo = new ProcessStartInfo
+                                using (var process = new Process())
                                 {
-                                    FileName = batchFilePath,
-                                    WorkingDirectory = workingDirectory,
-                                    RedirectStandardOutput = true,
-                                    RedirectStandardError = true,
-                                    UseShellExecute = false,
-                                    CreateNoWindow = true
-                                };
-
-                                // Starte den Prozess
-                                process.StartInfo = startInfo;
-                                process.Start();
-
-                                // Warte, bis der Prozess beendet ist
-                                process.WaitForExit();
-
-                                // Lese die Ausgabe des Prozesses
-                                string output = process.StandardOutput.ReadToEnd();
-                                string error = process.StandardError.ReadToEnd();
-
-                                // Gib die Ausgabe in die Konsole aus
-                                Console.WriteLine($"Ausgabe des Prozesses: {output}");
-                                Console.WriteLine($"Fehler des Prozesses: {error}");
+                                    var exePath = @"C:\Users\bks\Downloads\console.bat";
+                                    if (!string.IsNullOrEmpty(exePath))
+                                        ExecuteExe(exePath);
+                                    else
+                                        Console.WriteLine("Ungültiger Pfad. Das Programm wird beendet.");
+                                }
                             }
+
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"Fehler beim Ausführen der Batch-Datei: {ex.Message}");
+                            } 
                         }
-                        catch (Exception ex)
+                        else if (parameterValue == "stopServer")
                         {
-                            Console.WriteLine($"Fehler beim Ausführen der Batch-Datei: {ex.Message}");
+                            SendKeysToConsole("Command zum Stoppen des Servers = 'stop'");
                         }
-
-                        // Erstelle eine einfache Antwort mit mehr Informationen
-                        string responseString = "Hallo, dies ist dein Server!\n";
-                        responseString += $"Angeforderte URL: {context.Request.Url}\n";
-                        responseString += $"HTTP-Methode: {context.Request.HttpMethod}\n";
-
-                        byte[] buffer = Encoding.UTF8.GetBytes(responseString);
-
-                        // Sende die Antwort zurück an den Client
-                        context.Response.ContentLength64 = buffer.Length;
-                        context.Response.OutputStream.Write(buffer, 0, buffer.Length);
-                        context.Response.Close();
                     });
                 }
             }
@@ -96,10 +68,59 @@ class Program
             }
             finally
             {
-                // Stoppe den Server, wenn er nicht mehr benötigt wird
                 listener.Stop();
                 Console.WriteLine("Server gestoppt.");
             }
+        }
+    }
+    
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    public static void SendKeysToConsole(string text)
+    {
+        IntPtr hWnd = FindWindow(null, "Server");
+        if (hWnd != IntPtr.Zero)
+        {
+            foreach (char c in text)
+            {
+                PostMessage(hWnd, WM_CHAR, (IntPtr)c, IntPtr.Zero);
+            }
+            PostMessage(hWnd, WM_CHAR, (IntPtr)0x0D, IntPtr.Zero);
+        }
+        else
+        {
+            Console.WriteLine("Fehler: Konnte das Fenster 'Server' nicht finden.");
+        }
+    }
+
+    private static void ExecuteExe(string path)
+    {
+        try
+        {
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = path,
+                UseShellExecute = true
+            };
+
+            using (var process = Process.Start(processStartInfo))
+            {
+                if (process != null)
+                {
+                    process.WaitForExit();
+                    var exitCode = process.ExitCode;
+                    Console.WriteLine($"Der Prozess wurde mit dem Exit-Code {exitCode} beendet.");
+                }
+                else
+                {
+                    Console.WriteLine("Fehler beim Starten des Prozesses.");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fehler: {ex.Message}");
         }
     }
 }
